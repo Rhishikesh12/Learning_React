d5894fc3d1908d0b7e4ca6187b3f9382d8a82189
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-09-29 23:59:33 +0530

    third Project

--- basic_counter/src/App.jsx
+++ basic_counter/src/App.jsx
@@ -1,18 +1,25 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-	let [counter, setCounter] = useState(0);
+	const [counter, setCounter] = useState(0);
 
 	const addValue = () => {
 		if (counter < 20) {
+			setCounter(counter + 1);
+			setCounter(counter + 1);
+			setCounter(counter + 1);
 			setCounter(counter + 1);
 		}
 	};
 
 	const removeValue = () => {
 		if (counter > 0) {
-			setCounter(counter - 1);
+			setCounter((prevCounter) => prevCounter - 1);
+			setCounter((prevCounter) => prevCounter - 1);
+			setCounter((prevCounter) => prevCounter - 1);
+			setCounter((prevCounter) => prevCounter - 1);
 		}
 	};
 
