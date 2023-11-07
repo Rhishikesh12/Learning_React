e226177f1c73c8ec9e36dce19cc46a6dc844eaaa
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-11-07 11:41:33 +0530

    working fine

---
+++ redux_project/src/component/Count.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Count = () => {
+	return (
+		<>
+			<div>
+				<h1>0</h1>
+			</div>
+		</>
+	);
+};
+
+export default Count;
