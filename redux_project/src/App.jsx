14a08176831b9972cdbc263e06628d86ecb77515
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-11-08 22:57:28 +0530

    push

--- redux_project/src/App.jsx
+++ redux_project/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Count from "./component/Count";
+import { useDispatch } from "react-redux";
 
 function App() {
+	const dispatch = useDispatch();
 	return (
 		<>
 			<h1>Counter Using Redux</h1>
-			<button>Increment</button>
+			<button onClick={() => dispatch({ type: "Increment" })}>Increment</button>
 			<Count />
-			<button>Decrement</button>
+			<button onClick={() => dispatch({ type: "Decrement" })}>Decrement</button>
 		</>
 	);
 }
--- redux_project/src/component/Count.jsx
+++ redux_project/src/component/Count.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Count = () => {
+	const Count = useSelector((state) => state);
 	return (
 		<>
 			<div>
-				<h1>0</h1>
+				<h1>{Count}</h1>
 			</div>
 		</>
 	);
